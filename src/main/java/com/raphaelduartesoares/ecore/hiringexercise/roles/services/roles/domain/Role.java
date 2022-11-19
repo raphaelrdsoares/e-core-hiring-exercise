aa8f4ed5e11c3b0436324e1575ec22c9894f8bea
Raphael Soares <raphael.duartesoares@gmail.com>
2022-11-19 20:52:04 -0300

    feat: improves role properties

--- src/main/java/com/raphaelduartesoares/ecore/hiringexercise/roles/services/roles/domain/Role.java
+++ src/main/java/com/raphaelduartesoares/ecore/hiringexercise/roles/services/roles/domain/Role.java
@@ -1,12 +1,35 @@
 package com.raphaelduartesoares.ecore.hiringexercise.roles.services.roles.domain;
 
-import lombok.Builder;
-import lombok.Data;
+import com.raphaelduartesoares.ecore.hiringexercise.roles.api.rest.roles.dtos.RequestRoleDto;
+import com.raphaelduartesoares.ecore.hiringexercise.roles.api.rest.roles.dtos.ResponseRoleDto;
+import com.raphaelduartesoares.ecore.hiringexercise.roles.services.roles.infrastructure.repositories.entities.EntityRole;
 
-@Data
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
 @Builder
+@EqualsAndHashCode
 public class Role {
 
-  private String code;
-  private String namen;
+    private String code;
+    private String name;
+    private boolean isDefault;
+
+    public static Role fromDto(RequestRoleDto requestRoleDto) {
+        return Role.builder().code(requestRoleDto.code).name(requestRoleDto.displayName)
+                .isDefault(requestRoleDto.isDefault).build();
+    }
+
+    public ResponseRoleDto toDto() {
+        return new ResponseRoleDto(code, name, isDefault);
+    }
+
+    public EntityRole toEntity() {
+        return new EntityRole(code, name, isDefault);
+    }
+
 }
